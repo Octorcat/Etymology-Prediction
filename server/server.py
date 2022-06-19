afef3f2514b22d800b1e1463193742579b134d01
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-19 14:23:41 -0700

    feat(server): Add client routes
    Add routes for  index,.html and static files.

--- server/server.py
+++ server/server.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+
+from flask_cors import CORS
 from services.classification.classifier import predict
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return send_from_directory("../client/dist", "index.html")
+
+
+@app.route("/<path:path>")
+def serve_static_files(path):
+    return send_from_directory("../client/dist", path)
 
 
 @app.route("/etymology/<word>", methods=["GET"])
