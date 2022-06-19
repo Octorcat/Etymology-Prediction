934a3fe61a924a3e0e64174d4c8d2d09514eac82
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-19 16:26:05 -0700

    feat(classifier): Add typing

--- server/services/classification/classifier.py
+++ server/services/classification/classifier.py
@@ -4,22 +4,22 @@ import math
 
 from .model.load_model import NB_CHARS, NB_CATEGORIES, ALL_CHARS, model
 
-
-def word_to_tensor(word):
+ 
+def word_to_tensor(word: str) -> torch.Tensor:
     tensor = torch.zeros(len(word), 1, NB_CHARS)
     for i, letter in enumerate(word):
         tensor[i][0][ALL_CHARS.index(letter)] = 1
     return tensor
 
 
-def run_rnn_model_prediction(word_tensor):
+def run_rnn_model_prediction(word_tensor: torch.Tensor) -> torch.Tensor:
     hidden = model.initHidden()
     for i in range(word_tensor.size()[0]):
         output, hidden = model(word_tensor[i], hidden)
     return output
 
 
-def tensor_to_etymology(out_tensor):
+def tensor_to_etymology(out_tensor: torch.Tensor) -> dict:
     etymology_pred_probs = []
     topv, topi = out_tensor.topk(NB_CATEGORIES, 1, True)
     for i in range(NB_CATEGORIES):
@@ -29,7 +29,7 @@ def tensor_to_etymology(out_tensor):
     return {k: v for d in etymology_pred_probs for k, v in d.items()}
 
 
-def predict(word):
+def predict(word: str) -> dict:
     in_tensor = word_to_tensor(word)
     out_tensor = run_rnn_model_prediction(in_tensor)
     word_etymology_pred_probs = tensor_to_etymology(out_tensor)
