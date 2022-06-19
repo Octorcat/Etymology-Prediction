bb32b97854e917f9b26958859f2fbda45b47fd99
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-19 16:46:47 -0700

    feat(server): Add typing

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, Response, send_from_directory
 
 from flask_cors import CORS
 from services.classification.classifier import predict
@@ -8,16 +8,16 @@ CORS(app)
 
 
 @app.route("/")
-def index():
+def index() -> Response:
     return send_from_directory("../client/dist", "index.html")
 
 
 @app.route("/<path:path>")
-def serve_static_files(path):
+def serve_static_files(path: str) -> Response:
     return send_from_directory("../client/dist", path)
 
 
 @app.route("/etymology/<word>", methods=["GET"])
-def predict_etymology_of(word: str):
+def predict_etymology_of(word: str) -> dict:
     word_etymology_pred_probs = predict(word)
     return {"etymology": word_etymology_pred_probs}
