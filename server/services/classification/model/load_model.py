00224d9b813e4e249a89faa99ee82ea29402bd78
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-19 20:26:02 -0700

    feat(load_model): Add typing

--- server/services/classification/model/load_model.py
+++ server/services/classification/model/load_model.py
@@ -1,9 +1,34 @@
+from typing import List
 import torch
 import torch.nn as nn
 import os
 
 
-ALL_CHARS = [
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.tanh = nn.Tanh()  # Added layer on original
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        hidden = self.tanh(hidden)
+        output = self.i2o(combined)
+        output = self.tanh(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+ALL_CHARS: List[str] = [
     "x",
     "u",
     "l",
@@ -34,36 +59,10 @@ ALL_CHARS = [
     "a",
     ".",
 ]
-NB_CHARS = len(ALL_CHARS)
-NB_HIDDEN = 34 * 2
-NB_CATEGORIES = 2
-
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
-
-        self.hidden_size = hidden_size
-
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        self.tanh = nn.Tanh()  # Added layer on original
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        combined = torch.cat((input, hidden), 1)
-        hidden = self.i2h(combined)
-        hidden = self.tanh(hidden)
-        output = self.i2o(combined)
-        output = self.tanh(output)
-        output = self.softmax(output)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
-
-
-MODEL_PTH_PATH = f"{os.getcwd()}/services/classification/model/model.pth"
-model = RNN(NB_CHARS, NB_HIDDEN, NB_CATEGORIES)
+NB_CHARS: int = len(ALL_CHARS)
+NB_HIDDEN: int = 34 * 2
+NB_CATEGORIES: int = 2
+MODEL_PTH_PATH: str = f"{os.getcwd()}/services/classification/model/model.pth"
+model: RNN = RNN(NB_CHARS, NB_HIDDEN, NB_CATEGORIES)
 model.load_state_dict(torch.load(MODEL_PTH_PATH))
 model.eval()
