f53bcf4ce2e3b56e6c202cf04f2206e9287da40b
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-19 20:47:01 -0700

    feat(server): Use flask_restx for api routes

--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, Response, send_from_directory
+from flask_restx import Resource, Api
 
 from flask_cors import CORS
 from services.classification.classifier import EtymologyPrediction
 
+api = Api()
+
 app = Flask(__name__)
+api.init_app(app)
 CORS(app)
 
 
@@ -17,7 +21,8 @@ def serve_static_files(path: str) -> Response:
     return send_from_directory("../client/dist", path)
 
 
-@app.route("/etymology/<word>", methods=["GET"])
-def predict_etymology_of(word: str) -> dict:
-    word_etymology_pred_probs = EtymologyPrediction.predict(word)
-    return {"etymology": word_etymology_pred_probs}
+@api.route("/etymology/<string:word>")
+class EtymologyPredictionRouter(Resource):
+    def get(self, word: str) -> dict:
+        word_etymology_pred_probs = EtymologyPrediction.predict(word)
+        return {"etymology": word_etymology_pred_probs}
