3d4523b17132439f67bb08d1d412ad5f2911da1a
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-20 21:35:18 -0700

    fix(server): Move api docs from / to /docs
    This to prevent url clashes between client and api routes

--- server/server.py
+++ server/server.py
@@ -1,25 +1,27 @@
 from flask import Flask, Response, send_from_directory
 from flask_restx import Resource, Api
-
 from flask_cors import CORS
 from services.classification.classifier import EtymologyPrediction
 from services.classification.model.load_model import ALL_CHARS as ALLOWED_CHARS
 
-api = Api()
 
 app = Flask(__name__)
-api.init_app(app)
 CORS(app)
+
+CLIENT_DIST_DIR = "../client/dist"
 
 
 @app.route("/")
 def index() -> Response:
-    return send_from_directory("../client/dist", "index.html")
+    return send_from_directory(CLIENT_DIST_DIR, "index.html")
 
 
 @app.route("/<path:path>")
 def serve_static_files(path: str) -> Response:
-    return send_from_directory("../client/dist", path)
+    return send_from_directory(CLIENT_DIST_DIR, path)
+
+
+api = Api(app, doc="/doc")
 
 
 @api.route("/etymology/<string:word>")
@@ -28,7 +30,7 @@ class EtymologyPredictionRouter(Resource):
         MAX_WORD_LEN = 34
         word = word.strip().lower()
         if len(word) > MAX_WORD_LEN:
-            error_msg = f'Word must be <= {MAX_WORD_LEN}.'
+            error_msg = f"Word must be <= {MAX_WORD_LEN}."
             api.abort(400, error_msg)
         elif not all(char in ALLOWED_CHARS for char in word):
             error_msg = f"Word must be contain English letters and punctuation only."
