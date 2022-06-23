a7153ab7e035f2b80dcb9a7af5ff669c3a05ec35
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-22 18:44:01 -0700

    feat(DataTable): Create table to show raw probs

---
+++ client/src/lib/output/DataTable.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let Germanic: number = 0.5;
+  export let Latin: number = 0.5;
+</script>
+
+<section id="raw-results_table">
+  <table>
+    <tr>
+      <th>Etymology</th>
+      <th>Prob</th>
+    </tr>
+    <tr>
+      <td>Germanic</td>
+      <td>{Germanic}</td>
+    </tr>
+    <tr>
+      <td>Latin</td>
+      <td>{Latin}</td>
+    </tr>
+  </table>
+</section>
+
+<style>
+  #raw-results_table {
+    width: min(80vw, 300px);
+    overflow-x: scroll;
+    border: 1px solid purple;
+  }
+</style>
