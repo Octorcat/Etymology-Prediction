07e842561370d4ddd980c926c0da72320db37619
incredibledevhades <nestorolivas.business@gmail.com>
2022-06-28 13:31:13 -0700

    feat(MainFooter): Init page footer

---
+++ client/src/lib/footer/MainFooter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const currentYear: number = new Date().getFullYear();
+</script>
+
+<footer id="main_footer">
+  <a id="author_link" href="https://www.frederickroman.com">
+    &copy; Frederick Roman {currentYear}
+  </a>
+</footer>
+
+<style lang="scss">
+  #main_footer {
+    text-align: center;
+    margin: 20px auto;
+    #author_link {
+      text-decoration: none;
+    }
+  }
+</style>
